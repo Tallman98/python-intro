6df8a8b892bb2ebacc74e2c11ce7b449ec7aa186
David Igbokwe <itskane77@gmail.com>
2022-06-10 11:03:47 +0100

    Add files via upload
    made it more responsive

--- main.py
+++ main.py
@@ -1,61 +1,53 @@
 import random
 
 while True:
-    choices = ["R", "P", "S"]
+    choices = ["r", "p", "s"]
     computer =random.choice(choices)
     player = None
 
     while player not in choices:
-        player = input("Pick an option from 'R', 'P', 'S' ")
+        player = input("Pick an option from 'r', 'p', 's' ")
 
         if player not in choices:
             print("Invalid choice")
 
-        if player == computer:
-            print("Computer:", computer)
-            print("Player:", player)
-            print("It's a tie")
-
-        elif player =="R":
-            if computer =="P":
+        elif player =="r":
+            if computer =="p":
                 print("Computer:", computer)
                 print("Player:", player)
                 print("You lose!")
-            if computer =="S":
+            if computer =="s":
                 print("Computer:", computer)
                 print("Player:", player)
                 print("You win!")
-            if computer =="R":
-                print("Computer:", computer)
-                print("Player:", player)
-                print("It's a tie!")
 
-        elif player =="P":
-            if computer =="P":
-                print("Computer:", computer)
-                print("Player:", player)
-                print("It's a tie!")
-            if computer =="S":
+        elif player =="p":
+            if computer =="s":
                 print("Computer:", computer)
                 print("Player:", player)
                 print("You lose!")
-            if computer =="R":
+            if computer =="r":
                 print("Computer:", computer)
                 print("Player:",player)
                 print("You win!")
 
-        elif player =="S":
-            if computer =="P":
+        elif player =="s":
+            if computer =="p":
                 print("Computer:", computer)
                 print("Player:", player)
                 print("You win!")
-            if computer =="S":
-                print("Computer:", computer)
-                print("Player:", player)
-                print("It's a tie!")
-            if computer =="R":
+            if computer =="r":
                 print("Computer:", computer)
                 print("Player:", player)
                 print("You lose!")
-                
+
+    if player == computer:
+            print("Computer:", computer)
+            print("Player:", player)
+            print("It's a tie")
+            continue
+
+    end_game = input("Do you wish to continue playing? (yes/no)")
+    if end_game == "no":
+        break
 
